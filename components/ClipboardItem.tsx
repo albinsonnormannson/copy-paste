1da56a30942d40bd9776d5b54977f85812e060d1
Albinson Normanson <albinsonnormanson@gmail.com>
2023-01-14 08:17:09 -0400

    changed the ClipboardItem type toa union type of local database and remote database clipboard data

--- components/ClipboardItem.tsx
+++ components/ClipboardItem.tsx
@@ -1,14 +1,12 @@
+import { ClipboardItem as LocalDatabaseClipboardItem } from "@prisma/client";
+import { ClipboardItem as RemoteDatabseClipboardItem } from "../prisma-client/remote-prisma-client";
 import React, { useRef, createRef } from "react";
 import { AiFillDelete, AiOutlineDelete } from "react-icons/ai";
 import { FaPaste, FaCopy } from "react-icons/fa";
 import { useClipboardContext } from "./context/ClipboardContext";
 import { TooltipElement, TooltipRefType, useTooltip, withTooltip } from "./Tooltip";
 
-export type ClipboardItem = {
-  id: any;
-  content: any;
-  // createdAt: Date;
-};
+export type ClipboardItem = LocalDatabaseClipboardItem | RemoteDatabseClipboardItem;
 
 export const ClipboardItem = ({ value }: { value: ClipboardItem }) => {
   const copyTooltipRef = useRef<TooltipElement>(null) as TooltipRefType;
@@ -25,7 +23,7 @@ export const ClipboardItem = ({ value }: { value: ClipboardItem }) => {
     copyTooltip.toggleTooltip();
   };
 
-  const handleDelete = async (id: string) => {
+  const handleDelete = async (id: number) => {
     fetch(`/api/clipboard/${id}`, {
       headers: {
         "Content-Type": "application/json",
@@ -58,7 +56,7 @@ export const ClipboardItem = ({ value }: { value: ClipboardItem }) => {
   return (
     <div className="flex py-2 px-4 mb-2 bg-orange-200 rounded-sm">
       <div className="max-w-[90%] break-words">{value.content}</div>
-      {/* <span className="text-sm text-green-300">{value.createdAt.toDateString()}</span> */}
+      <span className="text-sm text-green-300">{value.createdAt.toDateString()}</span>
       <div style={{ marginLeft: "auto" }} className="grid place-items-center">
         <CopyButtonWithTooltip ref={copyTooltipRef}>Copied</CopyButtonWithTooltip>
         <button
