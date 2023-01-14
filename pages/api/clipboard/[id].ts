44dd03c4b6f4455e217a89cf6eeba37f3341f859
Albinson Normanson <albinsonnormanson@gmail.com>
2023-01-14 08:18:31 -0400

    handled deleting a single local or remote database clipboard item

--- pages/api/clipboard/[id].ts
+++ pages/api/clipboard/[id].ts
@@ -1,7 +1,10 @@
 import { Prisma } from "@prisma/client";
 import { NextApiHandler, NextApiRequest } from "next";
 import { datasource } from "../../../utils/datasource";
-import { deleteClipboardItem } from "../controllers/clipboard.controller";
+import {
+  deleteClipboardItem,
+  deleteRemoteClipboardItem,
+} from "../controllers/clipboard.controller";
 
 type DeleteApiRequest = NextApiRequest & {
   // req.query: string
@@ -10,16 +13,21 @@ type DeleteApiRequest = NextApiRequest & {
 const handler: NextApiHandler = async (req, res) => {
   switch (req.method) {
     case "DELETE":
-      const { id } = req.query;
+      const { id, remote } = req.query;
       if (id == "undefined") {
         return;
       }
       try {
-      const deleted = await deleteClipboardItem(Number(id));
-      res.json(deleted);
-    } catch (e: any) {
-      res.status(404).json({message: e.meta?.cause})
-    }
+        let deleted;
+        if (remote) {
+          deleted = await deleteRemoteClipboardItem(Number(id));
+        } else {
+          deleted = await deleteClipboardItem(Number(id));
+        }
+        res.json(deleted);
+      } catch (e: any) {
+        res.status(404).json({ message: e.meta?.cause, remote });
+      }
       break;
   }
 };
