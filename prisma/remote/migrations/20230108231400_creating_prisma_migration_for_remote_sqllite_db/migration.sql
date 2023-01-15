1d15d4a3f45c80791706c51f653a6e1d53a96738
Albinson Normanson <albinsonnormanson@gmail.com>
2023-01-15 07:42:46 -0400

    deleted migrations folder for remote schema

--- prisma/remote/migrations/20230108231400_creating_prisma_migration_for_remote_sqllite_db/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "ClipboardItem" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "content" TEXT NOT NULL,
-    "visible" BOOLEAN NOT NULL DEFAULT true,
-    "remote" BOOLEAN NOT NULL DEFAULT false
-);
